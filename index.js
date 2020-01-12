33e287fa322deae27f682201c39677fbdfdfe9d3
Peyman Hosseini <peyman.66h@gmail.com>
2020-01-12 20:58:14 +0300

    start and pause done

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
   start = () => {
-    console.log("hello world");
+    this.tik();
+    this.interval = setInterval(this.tik, 1000);
+  };
+  pause = () => {
+    clearInterval(this.interval);
+  };
+  tik = () => {
+    const intervalTime = parseFloat(this.durationTime.value);
+    this.durationTime.value = intervalTime - 1;
   };
 }
 const duration = document.querySelector("#duration");
